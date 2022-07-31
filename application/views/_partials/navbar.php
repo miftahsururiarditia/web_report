993bf005315c028bf7a5cfea247c7334b4089cda
muhammadubaidillah <muhammadubaidillah6@gmail.com>
2022-07-31 20:10:36 +0700

    add template

--- application/views/_partials/navbar.php
+++ application/views/_partials/navbar.php
@@ -1,9 +1,40 @@
-<nav class="navbar">
+<aside class="left-sidebar">
+	<!-- Sidebar scroll-->
+	<div class="scroll-sidebar">
+		<!-- Sidebar navigation-->
+		<nav class="sidebar-nav">
+			<ul id="sidebarnav">
+				<li>
+					<a href="<?= site_url('report') ?>" class="waves-effect"><i class="fa fa-clock-o m-r-10" aria-hidden="true"></i>All Report</a>
+				</li>
+				<li>
+					<a href="<?= site_url('report/total_unit') ?>" class="waves-effect"><i class="fa fa-user m-r-10" aria-hidden="true"></i>Total Unit</a>
+				</li>
+				<li>
+					<a href="<?= site_url('report/grosscom_sales_consultant') ?>" class="waves-effect"><i class="fa fa-table m-r-10" aria-hidden="true"></i>Grosscom Sales Consultant</a>
+				</li>
+				<li>
+					<a href="<?= site_url('report/ina_office_revenue') ?>" class="waves-effect"><i class="fa fa-font m-r-10" aria-hidden="true"></i>Ina Office evenue</a>
+				</li>
+				<li>
+					<a href="<?= site_url('report/ina_principal_report') ?>" class="waves-effect"><i class="fa fa-globe m-r-10" aria-hidden="true"></i>Ina Principal Report</a>
+				</li>
+				<li>
+					<a href="<?= site_url('report/ina_sales_consultant') ?>" class="waves-effect"><i class="fa fa-columns m-r-10" aria-hidden="true"></i>Ina Sales Consultant</a>
+				</li>
+			</ul>
+		</nav>
+		<!-- End Sidebar navigation -->
+	</div>
+	<!-- End Sidebar scroll-->
+</aside>
+
+<!-- <nav class="navbar">
 	<a href="<?= site_url('report') ?>">All Report</a>
 	<a href="<?= site_url('report/total_unit') ?>">Total Unit</a>
 	<a href="<?= site_url('report/grosscom_sales_consultant') ?>">Grosscom Sales Consultant</a>
 	<a href="<?= site_url('report/ina_office_revenue') ?>">Ina Office evenue</a>
-    <a href="<?= site_url('report/ina_principal_report') ?>">Ina Principal Report</a>
-    <a href="<?= site_url('report/ina_sales_consultant') ?>">Ina Sales Consultant</a>
-    <!-- <a href="<?= site_url('auth/login') ?>" style="margin-left:auto">Login</a> -->
-</nav>
+	<a href="<?= site_url('report/ina_principal_report') ?>">Ina Principal Report</a>
+	<a href="<?= site_url('report/ina_sales_consultant') ?>">Ina Sales Consultant</a>
+	<!-- <a href="<?= site_url('auth/login') ?>" style="margin-left:auto">Login</a> -->
+</nav> -->
