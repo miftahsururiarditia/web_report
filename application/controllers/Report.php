135dd1cadee8e17fa0b69b49901ef03de883a986
miftaharditia <miftaharditia@gmail.com>
2022-08-09 05:21:54 +0700

    Optimize query and multiple worksheet

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -35,102 +35,270 @@ class Report extends CI_Controller {
 		$spreadsheet = new Spreadsheet();
 		$sheet = $spreadsheet->getActiveSheet();    
 		
-		// Buat sebuah variabel untuk menampung pengaturan style dari header tabel
-		$style_col = [
-			'font' => ['bold' => true], // Set font nya jadi bold
-			'alignment' => [
-				'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER, // Set text jadi ditengah secara horizontal (center)
-				'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER // Set text jadi di tengah secara vertical (middle)
-			],      
-			'borders' => [
-				'top' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN], // Set border top dengan garis tipis
-				'right' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN],  // Set border right dengan garis tipis
-				'bottom' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN], // Set border bottom dengan garis tipis
-				'left' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN] // Set border left dengan garis tipis
-			]
-		];
-		
-		// Buat sebuah variabel untuk menampung pengaturan style dari isi tabel
-		$style_row = [
-			'alignment' => [
-				'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER // Set text jadi di tengah secara vertical (middle)
-			],
-			'borders' => [
-				'top' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN], // Set border top dengan garis tipis
-				'right' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN],  // Set border right dengan garis tipis
-				'bottom' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN], // Set border bottom dengan garis tipis
-				'left' => ['borderStyle'  => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THIN] // Set border left dengan garis tipis
-			]
-		];
-		
-		$sheet->setCellValue('A1', "DATA POST"); // Set kolom A1 dengan tulisan "DATA POST"
-		$sheet->mergeCells('A1:E1'); // Set Merge Cell pada kolom A1 sampai E1
-		$sheet->getStyle('A1')->getFont()->setBold(true); // Set bold kolom A1
+		$sheet->setCellValue('A1', "HARCOURTS INDONESIA");
+		$sheet->setCellValue('A2', "FLOW TRANSAKSI");
+		// $sheet->mergeCells('A1:E1'); // Set Merge Cell pada kolom A1 sampai E1
+		$sheet->getStyle('A1:A2')->getFont()->setBold(true); // Set bold kolom A1
 		
 		// Buat header tabel nya pada baris ke 3
-		$sheet->setCellValue('A3', "ID"); // Set kolom A3 dengan tulisan "NO"
-		$sheet->setCellValue('B3', "Post Author"); // Set kolom B3 dengan tulisan "NIS"
-		$sheet->setCellValue('C3', "Post Date"); // Set kolom C3 dengan tulisan "NAMA"
-		$sheet->setCellValue('D3', "Post title"); // Set kolom D3 dengan tulisan "JENIS KELAMIN"
-		$sheet->setCellValue('E3', "Post Status"); // Set kolom E3 dengan tulisan "ALAMAT"
-		$sheet->setCellValue('F3', "Post Name"); // Set kolom E3 dengan tulisan "ALAMAT"
-		
-		// Apply style header yang telah kita buat tadi ke masing-masing kolom header
-		$sheet->getStyle('A3')->applyFromArray($style_col);
-		$sheet->getStyle('B3')->applyFromArray($style_col);
-		$sheet->getStyle('C3')->applyFromArray($style_col);
-		$sheet->getStyle('D3')->applyFromArray($style_col);
-		$sheet->getStyle('E3')->applyFromArray($style_col);
-		$sheet->getStyle('F3')->applyFromArray($style_col);
-		
-		// Panggil function view yang ada di SiswaModel untuk menampilkan semua data siswanya
-		$siswa = $this->report_model->get_data();
+		$sheet->setCellValue('A5', "NO");
+		$sheet->mergeCells('A5:A7');
+		$sheet->setCellValue('B5', "Sales Consultant");
+		$sheet->mergeCells('B5:C5');
+		$sheet->setCellValue('B6', "Sales Listing");
+		$sheet->setCellValue('C6', "Sales Selling");
+		$sheet->mergeCells('B6:B7');
+		$sheet->mergeCells('C6:C7');
+		$sheet->setCellValue('D5', "Harcourts Office");
+		$sheet->mergeCells('D5:D7');
+		$sheet->setCellValue('E5', "Alamat Property");
+		$sheet->mergeCells('E5:E7');
+		$sheet->setCellValue('F5', "Type Transaksi");
+		$sheet->mergeCells('F5:G5');
+		$sheet->setCellValue('F6', "Jual / Beli");
+		$sheet->setCellValue('G6', "Sewa");
+		$sheet->mergeCells('F6:F7');
+		$sheet->mergeCells('G6:G7');
+		$sheet->setCellValue('H5', "Type Pembayaran");
+		$sheet->mergeCells('H5:I5');
+		$sheet->setCellValue('H6', "Cash");
+		$sheet->mergeCells('H6:H7');
+		$sheet->setCellValue('I6', "KPR");
+		$sheet->mergeCells('I6:J6');
+		$sheet->setCellValue('I7', "Plafon");
+		$sheet->setCellValue('J7', "Bank");
+		$sheet->setCellValue('K5', "Nilai Transaksi");
+		$sheet->mergeCells('K5:L5');
+		$sheet->setCellValue('K6', "Include PPN (10%)");
+		$sheet->setCellValue('L6', "Exclude PPN");
+		$sheet->mergeCells('K6:K7');
+		$sheet->mergeCells('L6:L7');
+		$sheet->setCellValue('M5', "Type Kerjasama Pemasaran");
+		$sheet->mergeCells('M5:R5');
+		$sheet->setCellValue('M6', "Full");
+		$sheet->setCellValue('N6', "Cobrooke");
+		$sheet->setCellValue('O6', "Refferal");
+		$sheet->setCellValue('P6', "Lead");
+		$sheet->setCellValue('Q6', "Commercial");
+		$sheet->setCellValue('R6', "Lain");
+		$sheet->mergeCells('M6:M7');
+		$sheet->mergeCells('N6:N7');
+		$sheet->mergeCells('O6:O7');
+		$sheet->mergeCells('P6:P7');
+		$sheet->mergeCells('Q6:Q7');
+		$sheet->mergeCells('R6:R7');
+		$sheet->setCellValue('S5', "Net Transaksi");
+		$sheet->mergeCells('S5:S7');
+		$sheet->setCellValue('T5', "% Comm");
+		$sheet->mergeCells('T5:T7');
+		$sheet->setCellValue('U5', "UNIT");
+		$sheet->mergeCells('U5:W5');
+		$sheet->setCellValue('U6', "Jual/Beli");
+		$sheet->setCellValue('V6', "Sewa");
+		$sheet->setCellValue('W6', "Total");
+		$sheet->mergeCells('U6:U7');
+		$sheet->mergeCells('V6:V7');
+		$sheet->mergeCells('W6:W7');
+		$sheet->setCellValue('X5', "Gross Com");
+		$sheet->mergeCells('X5:Z5');
+		$sheet->setCellValue('X6', "Jual/Beli");
+		$sheet->setCellValue('Y6', "Sewa");
+		$sheet->setCellValue('Z6', "Total");
+		$sheet->mergeCells('X6:X7');
+		$sheet->mergeCells('Y6:Y7');
+		$sheet->mergeCells('Z6:Z7');
+		$sheet->setCellValue('AA5', "Net Com");
+		$sheet->mergeCells('AA5:AB5');
+		$sheet->setCellValue('AA6', "PPh 23 (2%)");
+		$sheet->setCellValue('AB6', "Comm");
+		$sheet->mergeCells('AA6:AA7');
+		$sheet->mergeCells('AB6:AB7');
+		$sheet->setCellValue('AC4', "TERBIT INVOICE");
+		$sheet->setCellValue('AC5', "ROYALTI (9%+ppn)");
+		$sheet->mergeCells('AC4:AG4');
+		$sheet->mergeCells('AC5:AG5');
+		$sheet->setCellValue('AC6', "Royalti");
+		$sheet->setCellValue('AD6', "PPN (10%)");
+		$sheet->setCellValue('AE6', "PPh 23 (15%)");
+		$sheet->mergeCells('AC6:AC7');
+		$sheet->mergeCells('AD6:AD7');
+		$sheet->mergeCells('AE6:AE7');
+		$sheet->setCellValue('AF6', "Materai");
+		$sheet->setCellValue('AF7', "(Invoice > 5jt)");
+		$sheet->setCellValue('AG6', "TOTAL");
+		$sheet->mergeCells('AG6:AG7');
+
+		$sheet->getStyle('A4:AG7')->getFont()->setBold(true);
+
+		$sheet2 = $spreadsheet->createSheet();
+		$sheet2->setCellValue('B1', "HARCOURTS INDONESIA");
+		$sheet2->setCellValue('B2', "National Sales Consultant");
+		$sheet2->setCellValue('A5', "NO");
+		$sheet2->setCellValue('B5', "Sales Consultant");
+		$sheet2->setCellValue('C5', "Harcourts");
+		$sheet2->setCellValue('D5', "Gross Com Jual");
+		$sheet2->setCellValue('E5', "Gross Com Sewa");
+		$sheet2->setCellValue('F5', "TOTAL");
+		$sheet2->getStyle('A1:F5')->getFont()->setBold(true);
+
+		$sheet3 = $spreadsheet->createSheet();
+		$sheet3->setCellValue('C5', "HARCOURTS INDONESIA - TOTAL UNIT");
+		$sheet3->mergeCells('C5:F5');
+		$sheet3->setCellValue('A8', "NO");
+		$sheet3->setCellValue('B8', "Sales Consultant");
+		$sheet3->setCellValue('C8', "Harcourts");
+		$sheet3->setCellValue('D8', "Unit Jual");
+		$sheet3->setCellValue('E8', "Unit Sewa");
+		$sheet3->setCellValue('F8', "TOTAL UNIT");
+		$sheet3->getStyle('A1:F8')->getFont()->setBold(true);
+
+		$sheet4 = $spreadsheet->createSheet();
+		$sheet4->setCellValue('A1', "HARCOURTS INDONESIA - OFFICE BY REVENUE");
+		$sheet4->mergeCells('A1:B1');
+		$sheet4->setCellValue('A4', "Harcourts");
+		$sheet4->setCellValue('B4', "Rank");
+		$sheet4->getStyle('A1:B4')->getFont()->setBold(true);
+
+		$sheet5 = $spreadsheet->createSheet();
+		$sheet5->setCellValue('A1', "HARCOURTS INDONESIA - SALES CONSULTANT BY REVENUE");
+		$sheet5->mergeCells('A1:D1');
+		$sheet5->setCellValue('A4', "Sales Consultant");
+		$sheet5->setCellValue('B4', "Harcourts");
+		$sheet5->setCellValue('C4', "Career Path");
+		$sheet5->setCellValue('D4', "Rank");
+		$sheet5->getStyle('A1:D4')->getFont()->setBold(true);
+
+		$sheet6 = $spreadsheet->createSheet();
+		$sheet6->setCellValue('A1', "HARCOURTS INDONESIA - PRINCIPAL BY REVENUE");
+		$sheet6->mergeCells('A1:C1');
+		$sheet6->setCellValue('A4', "Principal Name");
+		$sheet6->setCellValue('B4', "Harcourts");
+		$sheet6->setCellValue('C4', "Rank");
+		$sheet6->getStyle('A1:C4')->getFont()->setBold(true);
+
+		// $limit_per_page = 100;
+		// $offset = html_escape($this->input->get('per_page'));
+		$datum = $this->report_model->get_data_export();
 		
 		$no = 1; // Untuk penomoran tabel, di awal set dengan 1
-		$numrow = 4; // Set baris pertama untuk isi tabel adalah baris ke 4
-		foreach($siswa as $data) { // Lakukan looping pada variabel siswa
-			$sheet->setCellValue('A'.$numrow, $data->ID);
-			$sheet->setCellValue('B'.$numrow, $data->post_author);
-			$sheet->setCellValue('C'.$numrow, $data->post_date);
-			$sheet->setCellValue('D'.$numrow, $data->post_title);
-			$sheet->setCellValue('E'.$numrow, $data->post_status);
-			$sheet->setCellValue('F'.$numrow, $data->post_name);
+		$numrow = 8; // Set baris pertama untuk isi tabel adalah baris ke 4
+
+		$no2 = 1;
+		$numrow2 = 6;
+
+		$no3 = 1;
+		$numrow3 = 9;
+
+		// $row = $this->report_model->count_data_export();
+		// $sheet->insertNewRowBefore($numrow, $row);
+		foreach($datum as $data) {
+			$sheet->setCellValue('A'.$numrow, $no);
+			$sheet->setCellValue('B'.$numrow, $data->sales_listing);
+			$sheet->setCellValue('C'.$numrow, $data->sales_selling);
+			$sheet->setCellValue('D'.$numrow, $data->harcourts_office);
+			$sheet->setCellValue('E'.$numrow, $data->alamat_property);
+			$sheet->setCellValue('F'.$numrow, $data->tipe_jual);
+			$sheet->setCellValue('G'.$numrow, $data->tipe_sewa);
+			$sheet->setCellValue('H'.$numrow, '');
+			$sheet->setCellValue('I'.$numrow, '');
+			$sheet->setCellValue('J'.$numrow, '');
+			$sheet->setCellValue('K'.$numrow, $data->include_ppn);
+			$sheet->setCellValue('L'.$numrow, '=+K'.$numrow.'/1.1');
+			$sheet->setCellValue('M'.$numrow, '');
+			$sheet->setCellValue('N'.$numrow, '');
+			$sheet->setCellValue('O'.$numrow, '');
+			$sheet->setCellValue('P'.$numrow, '');
+			$sheet->setCellValue('Q'.$numrow, '');
+			$sheet->setCellValue('R'.$numrow, '');
+			$sheet->setCellValue('S'.$numrow, '=+L'.$numrow.'*M'.$numrow);
+			$sheet->setCellValue('T'.$numrow, '');
+			$sheet->setCellValue('U'.$numrow, '');
+			$sheet->setCellValue('V'.$numrow, '');
+			$sheet->setCellValue('W'.$numrow, '');
+			$sheet->setCellValue('X'.$numrow, '');
+			$sheet->setCellValue('Y'.$numrow, '');
+			$sheet->setCellValue('Z'.$numrow, '=+X'.$numrow.'+Y'.$numrow);
+			$sheet->setCellValue('AA'.$numrow, '=+X'.$numrow.'*2%');
+			$sheet->setCellValue('AB'.$numrow, '=+Z'.$numrow.'+AA'.$numrow);
+			$sheet->setCellValue('AC'.$numrow, '=+AB'.$numrow.'*9%');
+			$sheet->setCellValue('AD'.$numrow, '=+AC'.$numrow.'*10%');
+			$sheet->setCellValue('AE'.$numrow, '=-AC'.$numrow.'*15%');
+			$sheet->setCellValue('AF'.$numrow, '');
+			$sheet->setCellValue('AG'.$numrow, '=+AC'.$numrow.'+AD'.$numrow.'+AE'.$numrow);
+
+			$sheet2->setCellValue('A'.$numrow2, $no2);
+			$sheet2->setCellValue('B'.$numrow2, $data->sales_selling);
+			$sheet2->setCellValue('C'.$numrow2, $data->harcourts_office);
+			$sheet2->setCellValue('D'.$numrow2, "='All Report'!X".$numrow);
+			$sheet2->setCellValue('E'.$numrow2, "='All Report'!Y".$numrow);
+			$sheet2->setCellValue('F'.$numrow2, '=+D'.$numrow2.'+E'.$numrow2);
 			
-			// Apply style row yang telah kita buat tadi ke masing-masing baris (isi tabel)
-			$sheet->getStyle('A'.$numrow)->applyFromArray($style_row);
-			$sheet->getStyle('B'.$numrow)->applyFromArray($style_row);
-			$sheet->getStyle('C'.$numrow)->applyFromArray($style_row);
-			$sheet->getStyle('D'.$numrow)->applyFromArray($style_row);
-			$sheet->getStyle('E'.$numrow)->applyFromArray($style_row);
-			$sheet->getStyle('F'.$numrow)->applyFromArray($style_row);
+			$sheet3->setCellValue('A'.$numrow3, $no3);
+			$sheet3->setCellValue('B'.$numrow3, $data->sales_selling);
+			$sheet3->setCellValue('C'.$numrow3, $data->harcourts_office);
+			$sheet3->setCellValue('D'.$numrow3, "='All Report'!U".$numrow);
+			$sheet3->setCellValue('E'.$numrow3, "='All Report'!V".$numrow);
+			$sheet3->setCellValue('F'.$numrow3, '=+D'.$numrow3.'+E'.$numrow3);
 			
 			$no++; // Tambah 1 setiap kali looping
 			$numrow++; // Tambah 1 setiap kali looping
+			$no2++;
+			$numrow2++;
+			$no3++;
+			$numrow3++;
 		}
+
+		$sheet->setCellValue('D'.++$numrow, 'Total');
+		$sheet->setCellValue('K'.$numrow, '=SUM(K8:K'.$numrow.')');
+		$sheet->setCellValue('L'.$numrow, '=SUM(L8:L'.$numrow.')');
+		$sheet->setCellValue('S'.$numrow, '=SUM(S8:S'.$numrow.')');
+		$sheet->setCellValue('U'.$numrow, '=SUM(U8:U'.$numrow.')');
+		$sheet->setCellValue('V'.$numrow, '=SUM(V8:V'.$numrow.')');
+		$sheet->setCellValue('W'.$numrow, '=SUM(W8:W'.$numrow.')');
+		$sheet->setCellValue('X'.$numrow, '=SUM(X8:X'.$numrow.')');
+		$sheet->setCellValue('Y'.$numrow, '=SUM(Y8:Y'.$numrow.')');
+		$sheet->setCellValue('Z'.$numrow, '=SUM(Z8:Z'.$numrow.')');
+		$sheet->setCellValue('AA'.$numrow, '=SUM(AA8:AA'.$numrow.')');
+		$sheet->setCellValue('AB'.$numrow, '=SUM(AB8:AB'.$numrow.')');
+		$sheet->setCellValue('AC'.$numrow, '=SUM(AC8:AC'.$numrow.')');
+		$sheet->setCellValue('AD'.$numrow, '=SUM(AD8:AD'.$numrow.')');
+		$sheet->setCellValue('AE'.$numrow, '=SUM(AE8:AE'.$numrow.')');
+		$sheet->setCellValue('AF'.$numrow, '=SUM(AF8:AF'.$numrow.')');
+		$sheet->setCellValue('AG'.$numrow, '=SUM(AG8:AG'.$numrow.')');
+		$sheet->getStyle('D'.$numrow.':AG'.$numrow)->getFont()->setBold(true);
 		
-		// Set width kolom
-		$sheet->getColumnDimension('A')->setWidth(5); // Set width kolom A
-		$sheet->getColumnDimension('B')->setWidth(15); // Set width kolom B
-		$sheet->getColumnDimension('C')->setWidth(25); // Set width kolom C
-		$sheet->getColumnDimension('D')->setWidth(20); // Set width kolom D
-		$sheet->getColumnDimension('E')->setWidth(30); // Set width kolom E
-		$sheet->getColumnDimension('F')->setWidth(30); // Set width kolom E
+		$sheet2->setCellValue('C'.$numrow2, 'Total');
+		$sheet2->setCellValue('D'.$numrow2, '=SUM(D6:D'.$numrow2.')');
+		$sheet2->setCellValue('E'.$numrow2, '=SUM(E6:E'.$numrow2.')');
+		$sheet2->setCellValue('F'.$numrow2, '=SUM(F6:F'.$numrow2.')');
+		$sheet2->getStyle('C'.$numrow2.':F'.$numrow2)->getFont()->setBold(true);
 		
-		// Set height semua kolom menjadi auto (mengikuti height isi dari kolommnya, jadi otomatis)
-		$sheet->getDefaultRowDimension()->setRowHeight(-1);
+		$sheet3->setCellValue('C'.$numrow3, 'Total');
+		$sheet3->setCellValue('D'.$numrow3, '=SUM(D9:D'.$numrow3.')');
+		$sheet3->setCellValue('E'.$numrow3, '=SUM(E9:E'.$numrow3.')');
+		$sheet3->setCellValue('F'.$numrow3, '=SUM(F9:F'.$numrow3.')');
+		$sheet3->getStyle('C'.$numrow3.':F'.$numrow3)->getFont()->setBold(true);
 		
 		// Set orientasi kertas jadi LANDSCAPE
-		$sheet->getPageSetup()->setOrientation(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::ORIENTATION_LANDSCAPE);
+		// $sheet->getPageSetup()->setOrientation(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::ORIENTATION_LANDSCAPE);
 		
 		// Set judul file excel nya
-		$sheet->setTitle("Laporan Data");
+		$sheet->setTitle("All Report");
+		$sheet2->setTitle("GrossCom Sales Consultant");
+		$sheet3->setTitle("Total Unit");
+		$sheet4->setTitle("Ina-Office by Revenue");
+		$sheet5->setTitle("Ina Sales Consultant");
+		$sheet6->setTitle("Ina - Principal Report");
+
+		$spreadsheet->setActiveSheetIndex(0);
 		
 		// Proses file excel
 		header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
 		header('Content-Disposition: attachment; filename="Data Laporan.xlsx"'); // Set nama file excel nya
-		header('Cache-Control: max-age=0');
+		// header('Cache-Control: max-age=0');
 		
 		$writer = new Xlsx($spreadsheet);
+		$writer->setPreCalculateFormulas(false);
 		$writer->save('php://output');
 	}
 }
--- application/models/Report_model.php
+++ application/models/Report_model.php
@@ -14,4 +14,35 @@ class Report_model extends CI_Model {
 		return $sql->row();
 	}
 
+	public function get_data_export() {
+		$sql = $this->db->query("SELECT b.organisational_unit_name as sales_listing, COALESCE(c.user_nicename, '') as sales_selling, b.organisational_unit_name as harcourts_office,
+		b.property_address as alamat_property, CASE WHEN b.jenis_listing = 'Jual' THEN 'X' ELSE '' END as tipe_jual, 
+		CASE WHEN b.jenis_listing = 'Sewa' THEN 'X' ELSE '' END as tipe_sewa, b.property_price as include_ppn
+		FROM wpgy_posts a
+		LEFT JOIN (
+			SELECT post_id, MAX(CASE WHEN meta_key = 'property_address' THEN meta_value ELSE '' END) as property_address,
+			MAX(CASE WHEN meta_key = 'OrganisationalUnitName' THEN meta_value ELSE '' END) as organisational_unit_name,
+			MAX(CASE WHEN meta_key = 'jenis_listing' THEN meta_value ELSE '' END) as jenis_listing,
+			MAX(CASE WHEN meta_key = 'property_price' THEN meta_value ELSE '' END) as property_price,
+			MAX(CASE WHEN meta_key = 'property_agent' THEN meta_value ELSE '' END) as property_agent
+			FROM wpgy_postmeta
+			WHERE post_id IN (
+				SELECT ID FROM wpgy_posts WHERE DATE(post_date) = '2022-06-05' AND post_type = 'estate_property'
+			) AND meta_key IN ('property_address', 'OrganisationalUnitName', 'jenis_listing', 'property_price', 'property_agent')
+			GROUP BY post_id
+		) b ON a.ID = b.post_id
+		LEFT JOIN wpgy_users c ON b.property_agent = c.ID
+		WHERE DATE(a.post_date) = '2022-06-05' AND a.post_type = 'estate_property'");
+
+		return $sql->result();
+	}
+
+	public function count_data_export() {
+		$sql = $this->db->query("SELECT COUNT(*) as jumlah
+				FROM wpgy_posts
+				WHERE DATE(post_date) = '2022-06-05' AND post_type = 'estate_property'");
+		
+		return $sql->row()->jumlah;
+	}
+
 }
