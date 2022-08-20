d98d26c45aaecbc9255166a47f81e3a5e2f2a183
muhammadubaidillah <muhammadubaidillah6@gmail.com>
2022-08-20 16:17:47 +0700

    add user role

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -14,37 +14,91 @@ class Report extends CI_Controller
 
     public function index()
     {
-        $data['meta'] = ['title' => 'All Report', 'user' => 'Administrator'];
+        $login_data = $this->session->userdata('session_data');
+
+        $role_data = array (
+            '1' => 'Admin', 
+            '2' => 'Agency',
+            '3' => 'Agent'
+        );
+
+        $user_role = $role_data[$login_data['user_role']];
+        $data['meta'] = ['title' => 'All Report', 'user' => $login_data['username'], 'role' => $user_role];
         $this->load->view('all_report', $data);
     }
 
     public function total_unit()
     {
-        $data['meta'] = ['title' => 'Total Unit', 'user' => 'Administrator'];
+        $login_data = $this->session->userdata('session_data');
+
+        $role_data = array (
+            '1' => 'Admin', 
+            '2' => 'Agency',
+            '3' => 'Agent'
+        );
+
+        $user_role = $role_data[$login_data['user_role']];
+        $data['meta'] = ['title' => 'All Report', 'user' => $login_data['username'], 'role' => $user_role];
         $this->load->view('total_unit', $data);
     }
 
     public function grosscom_sales_consultant()
     {
-        $data['meta'] = ['title' => 'Grosscom Sales Consultant', 'user' => 'Administrator'];
+        $login_data = $this->session->userdata('session_data');
+
+        $role_data = array (
+            '1' => 'Admin', 
+            '2' => 'Agency',
+            '3' => 'Agent'
+        );
+
+        $user_role = $role_data[$login_data['user_role']];
+        $data['meta'] = ['title' => 'All Report', 'user' => $login_data['username'], 'role' => $user_role];
         $this->load->view('grosscom_sales_consultant', $data);
     }
 
     public function ina_office_revenue()
     {
-        $data['meta'] = ['title' => 'Ina Office Revenue', 'user' => 'Administrator'];
+        $login_data = $this->session->userdata('session_data');
+        
+        $role_data = array (
+            '1' => 'Admin', 
+            '2' => 'Agency',
+            '3' => 'Agent'
+        );
+
+        $user_role = $role_data[$login_data['user_role']];
+        $data['meta'] = ['title' => 'All Report', 'user' => $login_data['username'], 'role' => $user_role];
         $this->load->view('ina_office_revenue', $data);
     }
 
     public function ina_principal_report()
     {
-        $data['meta'] = ['title' => 'Ina Principal Report', 'user' => 'Administrator'];
+        $login_data = $this->session->userdata('session_data');
+        
+        $role_data = array (
+            '1' => 'Admin', 
+            '2' => 'Agency',
+            '3' => 'Agent'
+        );
+
+        $user_role = $role_data[$login_data['user_role']];
+        $data['meta'] = ['title' => 'All Report', 'user' => $login_data['username'], 'role' => $user_role];
         $this->load->view('ina_principal_report', $data);
     }
 
     public function ina_sales_consultant()
     {
-        $data['meta'] = ['title' => 'Ina Sales Consultant', 'user' => 'Administrator'];
+        $login_data = $this->session->userdata('session_data');
+        
+        $role_data = array (
+            '1' => 'Admin', 
+            '2' => 'Agency',
+            '3' => 'Agent'
+        );
+
+        $user_role = $role_data[$login_data['user_role']];
+        $data['meta'] = ['title' => 'All Report', 'user' => $login_data['username'], 'role' => $user_role];
         $this->load->view('ina_sales_consultant', $data);
     }
 }
--- application/views/_partials/header.php
+++ application/views/_partials/header.php
@@ -8,12 +8,12 @@
         <nav class="navbar top-navbar navbar-toggleable-sm navbar-light">
             <div class="navbar-header">
                 <a class="navbar-brand" href="<?= site_url('all_report') ?>">
-                    <b>
-                        <img src="<?php echo base_url(); ?>assets/assets/images/logo-icon.png" alt="homepage" class="dark-logo" />
-                    </b>
                     <span>
-                        <img src="<?php echo base_url(); ?>assets/assets/images/logo-text.png" alt="homepage" class="dark-logo" />
+                        <img src="<?php echo base_url(); ?>assets/assets/images/harcourts-logo-small.png" alt="homepage" class="dark-logo" />
                     </span>
+                    <!-- <span>
+                        <img src="<?php echo base_url(); ?>assets/assets/images/logo-text.png" alt="homepage" class="dark-logo" />
+                    </span> -->
                 </a>
             </div>
             <div class="navbar-collapse">
@@ -21,14 +21,15 @@
                     <li class="nav-item"> <a class="nav-link nav-toggler hidden-md-up text-muted waves-effect waves-dark" href="javascript:void(0)"><i class="ti-menu"></i></a> </li>
                     <li class="nav-item hidden-sm-down">
                         <!-- <form class="app-search p-l-20"> -->
-                            <!-- <input type="text" class="form-control" placeholder="Search for...">  -->
-                            <!-- <a class="srh-btn"><i class="ti-search"></i></a> -->
+                        <!-- <input type="text" class="form-control" placeholder="Search for...">  -->
+                        <!-- <a class="srh-btn"><i class="ti-search"></i></a> -->
                         <!-- </form> -->
                     </li>
                 </ul>
                 <ul class="navbar-nav my-lg-0">
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="<?php echo base_url(); ?>assets/assets/images/users/1.jpg" alt="user" class="profile-pic m-r-5" /><?= isset($meta['user']) ? $meta['user'] : 'User' ?></a>
+                        <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="<?php echo base_url(); ?>assets/assets/images/users/profile.png" alt="user" class="profile-pic m-r-5" /><?= isset($meta['user']) ? $meta['user'] : 'User' ?></a>
+                        <a class="nav-link dropdown-toggle text-muted waves-effect waves-dark" href="" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?= isset($meta['role']) ? $meta['role'] : 'Role' ?></a>
                     </li>
                 </ul>
             </div>
@@ -36,5 +37,5 @@
     </header>
 
     <?php $this->load->view('_partials/navbar.php'); ?>
-    
+
 </div>
